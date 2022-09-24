2995ef85c2485576027f94d8d198e4956c07c65e
Eager-Blockchain-Dev <112506679+01blockchainDev@users.noreply.github.com>
2022-09-24 06:32:08 -0400

    Update AssemblyInfo.vb
    small change

--- Fringe Projection Simulator 2013/My Project/AssemblyInfo.vb
+++ Fringe Projection Simulator 2013/My Project/AssemblyInfo.vb
@@ -2,12 +2,6 @@
 Imports System.Reflection
 Imports System.Runtime.InteropServices
 
-' General Information about an assembly is controlled through the following 
-' set of attributes. Change these attribute values to modify the information
-' associated with an assembly.
-
-' Review the values of the assembly attributes
-
 <Assembly: AssemblyTitle("Fringe Projection Simulator 2013")> 
 <Assembly: AssemblyDescription("")> 
 <Assembly: AssemblyCompany("")> 
